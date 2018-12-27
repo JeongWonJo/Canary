0b0062214ca9454dcf03180a0ac49c6ae74771c4
JeongWonJo <31947445+JeongWonJo@users.noreply.github.com>
2018-12-28 02:41:57 +0900

    Update realtime_obstacle_detection.py

--- realtime_obstacle_detection.py
+++ realtime_obstacle_detection.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import os, glob
 import numpy as np
 import tensorflow as tf
-import subprocess 
-import os
+import subprocess
 import sys
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -18,9 +17,11 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640,480))
 #camera.start_preview()
 
-modelFullPath = '/home/pi/Desktop/output/output_graph.pb'                                      # 읽어들일 graph 파일 경로
-labelsFullPath = '/home/pi/Desktop/output/output_labels.txt'                                   # 읽어들일 labels 파일 경로
+modelFullPath = '/home/pi/Desktop/output/output_graph.pb'         # the path to the training model
+labelsFullPath = '/home/pi/Desktop/output/output_labels.txt'      # the path to the text file that contains labels
 
+
+# the function that creates the audio file (notices the user of the obstacle)
 def say(words):
     tempfile="temp.wav"
     devnull=open("/dev/null","w")
@@ -28,6 +29,8 @@ def say(words):
     subprocess.call(["aplay",tempfile],stderr=devnull)
     os.remove(tempfile)
 
+
+# read transferred trained model
 def create_graph():
     with tf.gfile.FastGFile(modelFullPath, 'rb') as f:
         graph_def = tf.GraphDef()
@@ -35,12 +38,14 @@ def create_graph():
         _ = tf.import_graph_def(graph_def, name='')
 
 
+# distinguish whether the tactile pavement block is linear/circular in real-time
 def run_inference_on_image():
     answer = None
     camera.capture("/home/pi/Desktop/temp_img.jpg")
     camera.start_preview()
     imagePath = "/home/pi/Desktop/temp_img.jpg"
-
+    
+    # if obstacles are not detected
     if not tf.gfile.Exists(imagePath):
         tf.logging.fatal('File does not exist %s', imagePath)
         return answer
@@ -48,15 +53,17 @@ def run_inference_on_image():
     image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
 
     create_graph()
-
+    
+    # if an obstacle is detected, recognize it
     with tf.Session() as sess:
-
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
         predictions = sess.run(softmax_tensor,
                                {'DecodeJpeg/contents:0': image_data})
         predictions = np.squeeze(predictions)
-
-        top_k = predictions.argsort()[-1:][::-1] 
+        
+        # sort prediction rates for each label from highest to lowest to recognize an obstacle
+        top_k = predictions.argsort()[-1:][::-1]
+        
         f = open(labelsFullPath, 'rb')
         lines = f.readlines()
         labels = [str(w).replace("\n", "") for w in lines]
@@ -64,7 +71,8 @@ def run_inference_on_image():
             human_string = labels[node_id]
             score = predictions[node_id]
             #print('%s (score = %.5f)' % (human_string, score))
-
+        
+        # obstacle recognized... aurally notifies the user
         answer = labels[top_k[0]]
         if "stair" in human_string:
             say("There are stairs nearby.")
